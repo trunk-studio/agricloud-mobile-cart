a8cbfef85d284dc97cc90ccf5c1998ee40be6323
fuyaode <dan82625@gmail.com>
2015-08-27 00:26:38 +0800

    add sync btn features

--- app/js/index.order.js
+++ app/js/index.order.js
@@ -10,6 +10,30 @@ $( document ).delegate("#order", "pageshow", function() {
 		$('#syncInfo').slideDown(300);
 	});
 
+  var showPurchaseList = function(list){
+    $('#orderStatusList').empty();
+    $.each(list, function (i) {
+        try{
+            $('#orderStatusList').append(
+            '<li class=\"ui-li-has-thumb ui-last-child\">'+
+                '<a href=\"#orderStatus\" rel=\"external\" class=\"ui-btn ui-btn-icon-right ui-icon-carat-r\">'+
+                '<img src=\"img/blackcat.jpg\" />'+
+                '<h3>雲端文旦禮盒</h3>'+
+                '<p>訂單日期：'+ list[i].createdAt +'</p>'+
+                '<p>金額：$'+ list[i].priceSum +
+                '元、配送地址：'+ list[i].Shipment.address +'</p>'+
+            '</a></li>');
+        }catch(e){
+
+        }
+    });
+  };
+
+  var urlParam = function(name){
+    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+    return results === null ? false : results[1];
+  };
+
   $("#sendOrderSyncRequestForm").on('submit',function(e){
     console.log("~~~~~~~~~~~~");
     e.preventDefault();
@@ -37,85 +61,24 @@ $( document ).delegate("#order", "pageshow", function() {
       data : postData,
       success:function(data, textStatus, jqXHR){
         console.log(data);
+        var list = JSON.parse(data).purchaseHistory
+        localStorage['purchaseHistory'] = JSON.stringify(list);
+        showPurchaseList(list);
       }
     });
   });
 
 
-
-
-
-	//方便測試用
-	// var purchaseHistorySpec = {
-	// 	purchaseHistory: [{
-	// 		createdAt: 'YYYYMMDD',
-	// 		priceSum: 1260,
-	// 		User:{
-	// 			id: 4,
-	// 			username: 'testOrderUser',
-	// 			email: 'smlsun@gmail.com',
-	// 			admin: false,
-	// 			createdAt: '2015-08-24T10:17:24.000Z',
-	// 			updatedAt: '2015-08-24T10:17:24.000Z',
-	// 			RoleId: null
-	// 		},
-	// 		Shipment: {
-	// 			username: '收件者1',
-	// 			mobile: '0922-222-222',
-	// 			taxId: '123456789',
-	// 			email: 'smlsun@gmail.com',
-	// 			address: '收件者的家'
-	// 		},
-	// 		OrderItems: [{
-	// 			name: '【特級】平安柚',
-	// 			description: '每箱六台斤 甜度 ★★★★★',
-	// 			quantity: 1,
-	// 			ProductId: 5
-	// 		}]
-	// 	},
-	// 	{
-	// 		createdAt: 'YYYYMMDD',
-	// 		priceSum: 1260,
-	// 		User:{
-	// 			id: 4,
-	// 			username: 'testOrderUser',
-	// 			email: 'smlsun@gmail.com',
-	// 			admin: false,
-	// 			createdAt: '2015-08-24T10:17:24.000Z',
-	// 			updatedAt: '2015-08-24T10:17:24.000Z',
-	// 			RoleId: null
-	// 		},
-	// 		Shipment: {
-	// 			username: '收件者2',
-	// 			mobile: '0922-222-222',
-	// 			taxId: '123456789',
-	// 			email: 'smlsun@gmail.com',
-	// 			address: '收件者的家'
-	// 		},
-	// 		OrderItems: [{
-	// 			name: '【特級】平安柚',
-	// 			description: '每箱六台斤 甜度 ★★★★★',
-	// 			quantity: 1,
-	// 			ProductId: 5
-	// 		}]
-	// 	}]
-	// };
-	// localStorage["purchaseHistory"] = JSON.stringify(purchaseHistorySpec);
-
-	$('#orderStatusList').empty();
   console.log('~~~~~~~~~~~~~~~~~~');
-	if(localStorage.purchaseHistory){
-		var list = JSON.parse(localStorage["purchaseHistory"]);
-		$.each(list, function (i) {
-			$('#orderStatusList').append(
-			'<li class=\"ui-li-has-thumb ui-last-child\">'+
-				'<a href=\"#orderStatus\" rel=\"external\" class=\"ui-btn ui-btn-icon-right ui-icon-carat-r\">'+
-				'<img src=\"img/blackcat.jpg\" />'+
-				'<h3>雲端文旦禮盒</h3>'+
-				'<p>訂單日期：'+ list[i].createdAt +'</p>'+
-				'<p>金額：$'+ list[i].priceSum +
-				'元、配送地址：'+ list[i].Shipment.address +'</p>'+
-			'</a></li>');
-		});
-	}
+  if(localStorage.purchaseHistory){
+    var list = JSON.parse(localStorage["purchaseHistory"]);
+    showPurchaseList(list);
+  }
+
+  var token = urlParam('token');
+  if(token){
+    $('#syncInfo').slideDown(300);
+    $('#token').val(token);
+  }
+
 });
